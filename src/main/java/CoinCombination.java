a489f0bc3c8c26961c92206979979f1bf80aeda7
farnoosh johnson and nick embrey <farnoosh@pdx.edu>
2016-04-21 11:33:20 -0700

    all the back-end testing passed

--- src/main/java/CoinCombination.java
+++ src/main/java/CoinCombination.java
@@ -6,16 +6,40 @@ public class CoinCombination {
   public static ArrayList<Object> runCoinCombination(String input) {
     ArrayList<Object> changeGiven = new ArrayList<Object>();
     Integer quartersGiven = 0;
-    Integer inputGiven = Integer.parseInt (input);
-  //  changeGiven.add(30);
-  //  changeGiven.add(20);
+    Integer quartersLeft = 10;
+    Integer dimesGiven = 0;
+    Integer dimesLeft = 10;
+    Integer nickelsGiven = 0;
+    Integer nickelsLeft = 20;
+    Integer penniesGiven = 0;
+    Integer inputGiven = Integer.parseInt(input);
+
   while(inputGiven > 0){
-    if (inputGiven >= 25){
+    //quarters
+    //return quartersGiven
+    //save in the system how many Qyarters left
+    //update the system with the num of quarters
+    if (quartersLeft > 0 && inputGiven >= 25){
       quartersGiven++;
+      quartersLeft--;
       inputGiven=inputGiven-25;
+    }else if (dimesLeft > 0 && inputGiven >= 10){
+      dimesGiven++;
+      dimesLeft--;
+      inputGiven=inputGiven-10;
+    }else if (nickelsLeft > 0 && inputGiven >= 5){
+      nickelsGiven++;
+      nickelsLeft--;
+      inputGiven=inputGiven-5;
+    } else {
+      penniesGiven = inputGiven;
+      inputGiven = 0;
     }
   }
     changeGiven.add(quartersGiven);
+    changeGiven.add(dimesGiven);
+    changeGiven.add(nickelsGiven);
+    changeGiven.add(penniesGiven);
     return changeGiven;
   }
 }
