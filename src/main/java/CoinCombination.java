f30a5ab45e84fe49feaa47597f48e6ab57a2353f
farnoosh johnson and nick embrey <cne.embrey@gmail.com>
2016-04-21 09:32:07 -0700

    Make getInputReturnEquivalentQuarters test to work

--- src/main/java/CoinCombination.java
+++ src/main/java/CoinCombination.java
@@ -3,7 +3,19 @@ import java.util.HashMap;
 
 
 public class CoinCombination {
-  public static Boolean runCoinCombination(String input) {
-    return true;
+  public static ArrayList<Object> runCoinCombination(String input) {
+    ArrayList<Object> changeGiven = new ArrayList<Object>();
+    Integer quartersGiven = 0;
+    Integer inputGiven = Integer.parseInt (input);
+  //  changeGiven.add(30);
+  //  changeGiven.add(20);
+  while(inputGiven > 0){
+    if (inputGiven >= 25){
+      quartersGiven++;
+      inputGiven=inputGiven-25;
+    }
+  }
+    changeGiven.add(quartersGiven);
+    return changeGiven;
   }
 }
--- src/test/java/coinCombinationTest.java
+++ src/test/java/coinCombinationTest.java
@@ -1,13 +1,33 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.util.ArrayList;
 
 public class coinCombinationTest{
 
 // the app takes the input and return the output
+//   @Test
+// public void runCoinCombination_getInputReturnOutput_ArrayList() {
+//   CoinCombination testCoinCombination = new CoinCombination();
+//   ArrayList<Object> expected = new ArrayList<Object>();
+//   expected.add(2);
+//   assertEquals(expected, testCoinCombination.runCoinCombination("50"));
+//   }
+
+  //the app gets the input and returns the euivalent in CoinCombination
   @Test
-public void runCoinCombination_getInputReturnOutput_true() {
+public void runCoinCombination_getInputReturnEquivalentQuarters_ArrayList() {
   CoinCombination testCoinCombination = new CoinCombination();
-  Boolean expected = true ;
+  ArrayList<Object> expected = new ArrayList<Object>();
+  expected.add(2);
   assertEquals(expected, testCoinCombination.runCoinCombination("50"));
   }
+
+  // @Test
+  // public void runCoinCombination_getInputReturnEquivalentDimes_ArrayList() {
+  //   CoinCombination testCoinCombination = new CoinCombination();
+  //   ArrayList<Object> expected = new ArrayList<Object>();
+  //   expected.add(2);
+  //   expected.add(3);
+  //   assertEquals(expected, testCoinCombination.runCoinCombination("80"));
+  // }
 }
